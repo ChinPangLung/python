3cb68ad0acf9fc61ce8e7e7b7c940b18ddaf993e
ChinPangLung <chinpang9527@gmail.com>
2018-10-10 10:34:24 +0800

    update

--- processTest/ProcessPool.py
+++ processTest/ProcessPool.py
@@ -6,7 +6,7 @@
 # @File    : ProcessPool.py
 # @Software: PyCharm
 from multiprocessing import Pool
-import time
+import time, datetime
 
 
 def foo(args):
@@ -15,8 +15,11 @@ def foo(args):
 
 
 if __name__ == '__main__':
+    start_time = datetime.datetime.now()
     pool = Pool(5)
     for i in range(30):
         pool.apply_async(func=foo, args=(i,))
     pool.close()  # 等子进程执行完后关闭进程池
     pool.join()
+    end_time = datetime.datetime.now()
+    print(end_time - start_time)
